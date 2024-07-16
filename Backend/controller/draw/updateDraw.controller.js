c968ca2d389d22a76c89bda55d96e8393c1abed5
ademyassikaya <adem_yassikaya@hotmail.com>
2024-07-17 01:31:05 +0300

    finish

--- Backend/controller/draw/updateDraw.controller.js
+++ Backend/controller/draw/updateDraw.controller.js
@@ -4,7 +4,18 @@ export default async function (req, res) {
   try {
     const id = req.params.id;
     const { name } = req.body;
-    const draw = await services.draw.updateDraw({ id, name });
+    const { kind } = req.body;
+    const { size } = req.body;
+    const { coordinates } = req.body;
+    const userId = req.userId;
+    const draw = await services.draw.updateDraw({
+      id,
+      name,
+      kind,
+      size,
+      coordinates,
+      userId,
+    });
     res.json(draw);
   } catch (error) {
     res.status(400).json({ message: error.message });
--- Backend/services/draw/updateDraw.service.js
+++ Backend/services/draw/updateDraw.service.js
@@ -1,13 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-export default async function ({ id, name }) {
+export default async function ({ id, name, kind, size, coordinates, userId }) {
   const prisma = new PrismaClient();
+  const transformedCoordinates = [];
+  for (let i = 0; i < coordinates.length; i += 2) {
+    transformedCoordinates.push([coordinates[i], coordinates[i + 1]]);
+  }
+  coordinates = JSON.stringify(transformedCoordinates);
   const draw = await prisma.draw.update({
     where: {
       id,
     },
     data: {
       name,
+      kind,
+      size,
+      coordinates,
+      userId,
     },
   });
   return draw;
